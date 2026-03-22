156a9187addcfd789f646692cb1fd966997b6091
Samuel Alvinge <alvinge@student.chalmers.se>
2026-03-22 11:15:53 +0100

    Upgraded main function

--- executables/main.cpp
+++ executables/main.cpp
@@ -1,14 +1,25 @@
 #include "logic/movegen/move_gen.h"
 #include "engine/perft/perft.h"
-// #include "engine/perft/perft.h"
 #include "io/board_printer.h"
 #include "logic/utils.h"
 
 #include <iostream>
 #include <chrono>
+#include <locale>
+
+namespace {
+
+struct space_separator : std::numpunct<char> {
+    char do_thousands_sep() const override { return ' '; }
+    std::string do_grouping() const override { return "\003"; }
+};
+
+} // namespace
 
 int main() 
 {
+    std::cout.imbue(std::locale(std::cout.getloc(), new space_separator()));
+
     /*
     Total number of nodes for depth 4: 6605806
 
@@ -86,39 +97,45 @@ int main()
 
     logic::attack_tables::init_attack_tables();
 
-    int const MAX_DEPTH = 4;
-
-    std::string startPos = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
-    std::string pos_two = "r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq -";
-    std::string pos_three = "8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - -";
-    std::string posFive = "rnbq1k1r/pp1Pbppp/2p5/8/2B5/8/PPP1NnPP/RNBQK2R w KQ - 1 8";
+    std::string start_pos  = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
+    std::string pos2  = "r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq -";
+    std::string pos3  = "8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - - 0 1";
+    std::string pos4  = "r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P2PP/R2Q1RK1 w kq - 0 1";
+    std::string pos5  = "rnbq1k1r/pp1Pbppp/2p5/8/2B5/8/PPP1NnPP/RNBQK2R w KQ - 1 8";
+    std::string pos6  = "r4rk1/1pp1qppp/p1np1n2/2b1p1B1/2B1P1b1/P1NP1N2/1PP1QPPP/R4RK1 w - - 0 10";
     
-    bool recPerftStats = false;
+    engine::Perft perft_start_pos = engine::Perft(5);
+    perft_start_pos.set_pos_from_fen(start_pos);
+    perft_start_pos.minimax(0, 0, false);
 
-    engine::Perft movePickerStartPos = engine::Perft(MAX_DEPTH);
-    movePickerStartPos.set_pos_from_fen(startPos);
-    movePickerStartPos.minimax(0, 0, recPerftStats);
-
-    engine::Perft movePickerPosTwo = engine::Perft(MAX_DEPTH);
-    movePickerPosTwo.set_pos_from_fen(pos_two);
-    movePickerPosTwo.minimax(0, 0, recPerftStats);
+    engine::Perft perft_pos2 = engine::Perft(4);
+    perft_pos2.set_pos_from_fen(pos2);
+    perft_pos2.minimax(0, 0, false);
     
-    engine::Perft movePickerPosThree = engine::Perft(MAX_DEPTH);
-    movePickerPosThree.set_pos_from_fen(pos_three);
-    movePickerPosThree.minimax(0, 0, recPerftStats);
+    engine::Perft perft_pos3 = engine::Perft(6);
+    perft_pos3.set_pos_from_fen(pos3);
+    perft_pos3.minimax(0, 0, false);
 
-    engine::Perft movePickerPosFive = engine::Perft(MAX_DEPTH);
-    movePickerPosFive.set_pos_from_fen(posFive);
-    movePickerPosFive.minimax(0, 0, recPerftStats);
+    engine::Perft perft_pos4 = engine::Perft(5);
+    perft_pos4.set_pos_from_fen(pos4);
+    perft_pos4.minimax(0, 0, false);
 
-    if (recPerftStats) {
-        int sum = 0;
-        sum += movePickerStartPos.sum_nodes_to_depth(MAX_DEPTH);
-        sum += movePickerPosTwo.sum_nodes_to_depth(MAX_DEPTH);
-        sum += movePickerPosThree.sum_nodes_to_depth(MAX_DEPTH);
-        sum += movePickerPosFive.sum_nodes_to_depth(MAX_DEPTH);
-        std::cout << "Total nodes: " << sum << std::endl;
-    }
+    engine::Perft perft_pos5 = engine::Perft(4);
+    perft_pos5.set_pos_from_fen(pos5);
+    perft_pos5.minimax(0, 0, false);
+
+    engine::Perft perft_pos6 = engine::Perft(4);
+    perft_pos6.set_pos_from_fen(pos6);
+    perft_pos6.minimax(0, 0, false);
+
+    long long sum = 0LL;
+    sum += perft_start_pos.sum_nodes_to_depth(5);
+    sum += perft_pos2.sum_nodes_to_depth(4);
+    sum += perft_pos3.sum_nodes_to_depth(6);
+    sum += perft_pos4.sum_nodes_to_depth(5);
+    sum += perft_pos5.sum_nodes_to_depth(4);
+    sum += perft_pos6.sum_nodes_to_depth(4);
+    std::cout << "Total nodes: " << sum << std::endl;
 
     // End clock
     auto end = std::chrono::high_resolution_clock::now();
@@ -126,9 +143,10 @@ int main()
     // Calculate duration
     std::chrono::duration<double> elapsed_seconds = end - start;
     std::cout << "Elapsed time: " << elapsed_seconds.count() << "s" << std::endl;
-    float nodesPerSecond = 6605806 / elapsed_seconds.count();
-    std::cout << "Nodes per second: " << nodesPerSecond << std::endl;
-    std::cout << "SF%: " << nodesPerSecond / 250000000 << std::endl;
+    int nodes_per_s = sum / elapsed_seconds.count();
+    float nodes_per_s_float = sum / elapsed_seconds.count();
+    std::cout << "Nodes per second: " << nodes_per_s << std::endl;
+    std::cout << "SF%: " << (nodes_per_s_float / 250000000) * 100 << std::endl;
 
     return 0;
 }
